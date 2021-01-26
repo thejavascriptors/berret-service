9a9f4dd9cbc1c41c9562fbb028ac70003f76ac7f
Berret Terry <berretterry@gmail.com>
2021-01-26 10:46:13 -0600

    rounded the pages up if there is a larger number of items in the database

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -23,7 +23,7 @@ class App extends React.Component {
       this.setState({
         products: data.docs,
         lastId: data.docs[4].prodNum,
-        pages: data.total / 5
+        pages: Math.ceil(data.total / 5)
       })
     })
   }
