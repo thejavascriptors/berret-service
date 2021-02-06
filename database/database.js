8916e9d77a378ed7e5afb72c3ca60a197d5f09d4
Nathan Wong <natdanwongielhan@yahoo.com>
2021-02-05 23:09:08 -0800

    Seeds now. not 10M yet

--- database/database.js
+++ database/database.js
@@ -1,6 +1,5 @@
 const {Pool, Client} = require('pg');
-const client = new Client();
-
+// const client = new Client();
 /*
 PGUSER='nathan' \
 PGHOST='localhost' \
@@ -10,52 +9,31 @@ node database/seed.js
 */
 const pool = new Pool({
   user: 'nathan',
-  host:'localhost',
+  host: 'localhost',
   database: 'postgres',
-  port: 55461,
+  port: 5432,
 })
-
-pool.connect( (err, client, release) => {
-   if(err){
-     console.log(err);
-   } else {
-     client.query('SELECT NOW()', (err,result) => {
-       release();
-       if(err){
-         console.log(err);
-       }
-     })
-     console.log('HELLOOOOO')
-   }
-});
-
-pool.query('CREATE TABLE (IF NOT EXISTS) PRODUCTS (NAME STRING, PRODNUM INT, PHOTOURL STRING, DESC STRING, RATING INT, REVIEW_COUNT INT, PRICE INT, PRIMARY KEY (PRODNUM)')
-  .then( ()=>{
-    console.log('here we are')
-    pool.query('CREATE INDEX product_index ON Product PRODNUM')
-      .then( ()=>{
-        console.log('done');
-      })
-      .catch( (err) => {
-        console.log(err)
-      })
-  })
-  .catch( (err) => {
-    console.log(err)
-  })
-
+// pool.query(`CREATE TABLE IF NOT EXISTS products (
+//     name VARCHAR (255),
+//     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//     photoURL VARCHAR (255),
+//     descript VARCHAR (255),
+//     rating INT,
+//     review_count INT,
+//     price INT
+//   );`)
+//   .then( ()=>{
+//     console.log('here we are')
+//     pool.query('CREATE INDEX product_index ON Product id')
+//       .then( ()=>{
+//         console.log('done');
+//       })
+//       .catch( (err) => {
+//         console.log('error creating products table', err)
+//       })
+//   })
+//   .catch( (err) => {
+//     console.log(err)
+//   })
 //function to readOne/All for API call
-
 module.exports.pool = pool;
-
-/*
-
-# Removes latest commit from the stash, KEEPS changes
-git reset --soft HEAD~
-
-# Removes latest commit from the stash, DELETES changes
-git reset --hard HEAD~
-
-git rebase --onto <branch name>~<first commit number to remove> <branch name>~<first commit to be kept> <branch name>
-
-*/
--- database/seed.js
+++ database/seed.js
@@ -3,17 +3,15 @@ const faker = require('faker');
 const db = database.pool;
 const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
-
-
 let seed = async () => {
   //let start
   let start = 1;
   let end = 1000000;
   let records = [];
   // loop to 10 when ready
-  for(let loop = 1; loop <= 1; loop++){
+  for(let loop = 1; loop <= 10; loop++){
     const csvWrite = createCsvWriter({
-      header: ['NAME', 'PRODNUM', 'PHOTOURL', 'DESC', 'RATING', 'REVIEW_COUNT', 'PRICE'],
+      header: ['name', 'id', 'photoURL', 'descript', 'rating', 'review_count', 'price'],
       path: `./database/csvFolder/data${loop}.csv`
     })
     for(let i = start; i <= end; i++){
@@ -23,21 +21,20 @@ let seed = async () => {
       let desc = faker.commerce.productDescription();
       let rating = Math.floor(Math.random() * Math.floor(5));
       let review_count = Math.floor(Math.random() * Math.floor(3000));
-      let price = faker.commerce.price(10, 200, 2, '$');
+      let price = Math.floor(Math.random() * Math.floor(1000));
       records.push([name,prodNum, photoUrl, desc, rating, review_count, price]);
     }
     csvWrite.writeRecords(records)
       .then( async () => {
-        await db.query(`COPY postgres("NAME", "PRODNUM", "PHOTOURL", "DESC", "RATING", "REVIEW_COUNT", "PRICE") FROM '/ndw001_SDC/database/csvFolder/data${loop}.csv' DELIMITER ',' CSV HEADER;`)
+        await db.query(`COPY products("name", "id", "photourl", "descript", "rating", "review_count", "price") FROM '/Users/nathan/Desktop/ndw001_SDC/database/csvFolder/data${loop}.csv' DELIMITER ',' CSV HEADER;`)
       })
       .catch( (err) => {
-        console.log(err);
+        console.log('error writing records from csv', err);
       })
     start += end;
     end += end;
     records = [];
   }
-
   // query from csv into database // copy path to
 }
 seed();
