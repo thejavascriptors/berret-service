d9bfbd2c070a8565be3729f4eb48f68c1ad62a85
Nathan Wong <natdanwongielhan@yahoo.com>
2021-02-06 13:36:51 -0800

    Server is working. Api get request for one product complete

--- server/index.js
+++ server/index.js
@@ -1,16 +1,36 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const database = require('../database/database.js');
+const db = database.pool;
 
 const cors = require('cors');
 const path = require('path');
-const port = 4000;
+const port = 8080;
 
-app.use(express.static(path.join(__dirname, '../public')))
+// app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.json());
 app.use(cors());
 
-// app.get('')
+app.get('/', (req,res) =>{
+  // console.log('hello there')
+  res.send('hello');
+})
 
+app.listen(port, () => {
+  console.log('listening at 8080');
+})
+
+app.get('/api/product/:id', (req,res) =>{
+  let sendData = (err, data) => {
+    if(err){
+      console.log(err);
+    } else {
+      console.log(data.rows)
+      res.send(data.rows);
+    }
+  }
+  db.readOne(req.params.id, sendData);
+})
 
 
 
